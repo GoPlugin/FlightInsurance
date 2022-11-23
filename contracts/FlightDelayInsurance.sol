d3357f1e8dd7b241eff27873e40a3e14f55e6585
raghu0891 <raghu0891@gmail.com>
2022-11-23 14:47:45 +0400

    Tested execpt submit&fulfill

--- contracts/FlightDelayInsurance.sol
+++ contracts/FlightDelayInsurance.sol
@@ -1,30 +1,19 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-//import "../CustomTokens/contracts/utils/Counters.sol";
+
 import "@goplugin/contracts/src/v0.8/Plugin.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@goplugin/contracts/src/v0.8/PluginClient.sol";
-//import "./PLI.sol";
 import "./interface/IFlightStatus.sol";
 
 contract FlightDelayInsurance is PluginClient, IFlightStatus {
-    // using Counters for Counters.Counter;
-    // Counters.Counter private _flightIds;
-    // Counters.Counter private _insuranceIds;
     
-    //uint256 public constant MIN_TIME_BEFORE_DEPARTURE = 5 * 24 hours;
     uint constant SECONDS_PER_DAY = 24 * 60 * 60;
     uint constant SECONDS_PER_HOUR = 60 * 60;
     uint constant SECONDS_PER_MINUTE = 60;
     int constant OFFSET19700101 = 2440588;
-    
-    // Maximum time before departure for applying
-    //uint256 public constant MAX_TIME_BEFORE_DEPARTURE = 60 * 24 hours;
     using Plugin for Plugin.Request;
-
-    //Plugin token instance
-    // PluginClient pluginToken; 
 
     uint256 private constant ORACLE_PAYMENT = 0.001 * 10**18;
      uint public fiveHourTimeStamp;
@@ -40,10 +29,7 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
 
     constructor(address _pli) {
         setPluginToken(_pli);
-        //setPublicPluginToken(_pli);
         owner = msg.sender;
-        // _flightIds.increment();
-        // _insuranceIds.increment();
     }
 
     modifier only_owner() {
@@ -51,10 +37,6 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
         _;
     }
 
-    // modifier onlyWhitelistedAirlines(address _airlineAddress) {
-    //     require(airline);
-    //     _;
-    // }
 
     event FlightEvents(
         uint256 flightId,
@@ -156,7 +138,7 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
             _passengerPnr,
             _airlineKey
         );
-        //passenger[passengerKey] = true;
+
         emit PassengerEvents(
             passengerKey,
             "Passenger Registered",
@@ -168,8 +150,19 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
     }
 
     //Passenger Utility
-    function getPassengerKey(address _passengerAddress,string memory _passengerPnr, bytes memory _airlineKey) pure internal returns(bytes memory) {
-        return abi.encodePacked('FDI-PA-',_passengerAddress,_passengerPnr, _airlineKey);
+    function getPassengerKey(address _passengerAddress,string memory _passengerPnr, bytes memory _airlineKey) internal returns(bytes memory) {
+        return extractBytes(abi.encodePacked('FDI-PA-',_passengerAddress,_passengerPnr, _airlineKey),6,11);
+    }
+    
+    function extractBytes (
+        bytes memory data, 
+        uint8 from, 
+        uint8 n) public returns(bytes memory) {
+      bytes memory returnValue = new bytes(n);
+      for (uint8 i = 0; i < n - from; i++) {
+        returnValue[i] = data[i + from]; 
+      }
+      return returnValue;
     }
 
     //Register Airlines
@@ -179,8 +172,7 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
         string memory _iataSymbol,
         string memory _airlineName
     ) public returns(bytes memory){
-        bytes memory airlineKey = getAirlineKey(_airlineAddress,_iataSymbol,_airlineName);
-        //require(airline[airlineKey] != _airlineAddress, "Airline exists");
+        bytes memory airlineKey = getAirlineKey(_iataSymbol,_airlineName);
         require(airline[airlineKey].airlineAddress != _airlineAddress, "ERROR:FDI-ALR-01:AIRLINE_REGISTERED");
         airline[airlineKey] = AirlineDetails(
             _airlineAddress,
@@ -200,36 +192,10 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
     }
 
     //Airlines Utility
-    function getAirlineKey(address _airlineAddress,string memory _iataSymbol, string memory _airlineName) pure internal returns(bytes memory) {
-        return abi.encodePacked('FDI-AL-',_airlineAddress,_iataSymbol,_airlineName);
+    function getAirlineKey(string memory _iataSymbol, string memory _airlineName) internal returns(bytes memory) {
+        return extractBytes(abi.encodePacked('FDI-AL-',_iataSymbol,_airlineName), 6, 11);
     }
-    // Register Flight
-    // function registerFlights(
-    //     address _flightAddress,
-    //     bytes32 _carrierFlightNumber,
-    //     string memory _serviceProviderName
-    // ) public returns (uint256) {
-    //     uint256 _flightId = _flightIds.current();
-    //     _flightIds.increment();
 
-    //     flights[_flightId][_flightAddress] = FlightMaster(
-    //         _flightId,
-    //         _carrierFlightNumber,
-    //         _serviceProviderName,
-    //         _flightAddress,
-    //         block.timestamp,
-    //         msg.sender
-    //     );
-    //     emit FlightEvents(
-    //         _flightId,
-    //         "Flight Registered",
-    //         msg.sender,
-    //         msg.sender,
-    //         block.timestamp
-    //     );
-
-    //     return _flightId;
-    // }
     // Airline stake fund for Insurance 
     //ERROR code: SAS
     function setAirlineStake(uint256 _stakeFund, address _airlineAddress, bytes memory _airlineKey) public returns(bool) {
@@ -259,16 +225,9 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
         uint256 _arrivalTimeStamp
 
     ) public returns (bytes memory) {
-        // uint256 _insuranceId = _insuranceIds.current();
-        // _insuranceIds.increment();
-        //require(insurances[_policyKey] == false, "Policy has already been issued");
-        // uint departTimeStamp;
-        // uint public fiveHourTimeStamp;
-        // uint public fiveDayTimeStamp;
-        
-        // (departTimeStamp, 
+    
         (fiveHourTimeStamp, fiveDayTimeStamp) = _timestampDepartDateTime(_departureTimeStamp);
-        // uint arrivalDateTime = _timestampArrivalDateTime(_departureTime);
+    
 
         require(_arrivalTimeStamp > _departureTimeStamp, "ERROR:FDI-BIS-01:ARRIVAL_BEFORE_DEPARTURE_TIME");
         require(block.timestamp < fiveHourTimeStamp, "ERROR:FDI-BIS-02:FLIGHT_DEPARTURE_LESS_THAN_5_HOURS");
@@ -277,7 +236,7 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
         bytes memory _policyKey = insuranceKeyGen(_passengerKey,_flightIcao,_airlineKey);
         
         insurances[_policyKey][passenger[_passengerKey].passengerAddress] = FlightInsurance(
-        //insurances[_insuranceId][_passengerAddress] = FlightInsurance(
+    
             _policyKey,
             _passengerKey,
             _flightIcao,
@@ -302,8 +261,8 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
     }
 
     //Insurance utility
-    function insuranceKeyGen(bytes memory _passengerKey,string memory _flightIcao, bytes memory _airlineKey) pure internal returns(bytes memory) {
-        return abi.encodePacked('FDI-POLICY-',_passengerKey,_flightIcao,_airlineKey);
+    function insuranceKeyGen(bytes memory _passengerKey,string memory _flightIcao, bytes memory _airlineKey) internal returns(bytes memory) {
+        return extractBytes(abi.encodePacked('FDI-POLICY-',_passengerKey,_flightIcao,_airlineKey),6,11);
     }
 
     //Claim Processing
@@ -311,45 +270,45 @@ contract FlightDelayInsurance is PluginClient, IFlightStatus {
     function submitMyClaim(
         bytes memory _policyKey,
         bytes memory _passengerKey,
-        address _oracleAddress,
-        uint256 _arrivalTime, //Need to be queried with avaiatiostack api about the flight arrival status using flight_icao and departure dateTime
-        string memory _jobid
+        // address _oracleAddress,
+        // uint256 _jobid,
+        uint256 _arrivalTime //Need to be queried with avaiatiostack api about the flight arrival status using flight_icao and departure dateTime
+    
     ) public {
         FlightInsurance memory fin = insurances[_policyKey][msg.sender];
         require(insurances[_policyKey][msg.sender].isExist == true, "ERROR:FDI-SMC-01:PPLICY_PASSENGER_KEY_NOT_FOUND");
         require(passenger[fin.passengerKey].passengerAddress != address(0),"ERROR:FDI-SMC-02:ZERO_ADDRESS");
-        // require(insurances[_policyKey][passenger[_passengerKey].passengerAddress].passengerKey == _passengerKey,"ERROR:FDI-SMC-02:ADDRESS_MISMATCH");
         require(fin.processed == false, "ERROR:FDI-SMC-03:POLICY_PROCESSED");
         require(fin.policystatus == PolicyStatus(0), "ERROR:FDI-SMC-04:POLICY_CLAIMED");
         require(fin.arrivalOn != _arrivalTime, "ERROR:FDI-SMC-05:ON-TIME_ARRIVAL");
 
-        Plugin.Request memory req = buildPluginRequest(
-            stringToBytes32(_jobid),
-            address(this),
-            this.fulfillClaimInquiry.selector
-        );
-        req.addUint("until", fin.arrivalOn);
-        bytes32 reqId = sendPluginRequestTo(
-            _oracleAddress,
-            req,
-            ORACLE_PAYMENT
-        );
-        claims[reqId] = fin;
-        claimRequest[_policyKey][msg.sender] = reqId;
-        fin.policystatus = PolicyStatus(1);
+        // Plugin.Request memory req = buildPluginRequest(
+        //     stringToBytes32(_jobid),
+        //     address(this),
+        //     this.fulfillClaimInquiry.selector
+        // );
+        // req.addUint("until", fin.arrivalOn);
+        // bytes32 reqId = sendPluginRequestTo(
+        //     _oracleAddress,
+        //     req,
+        //     ORACLE_PAYMENT
+        // );
+        // claims[reqId] = fin;
+        // claimRequest[_policyKey][msg.sender] = reqId;
+        // fin.policystatus = PolicyStatus(1);
 
-        emit submitMyClaimLog(reqId);
+        // emit submitMyClaimLog(reqId);
     }
 
     //Flight Status
     //Error code: FDC
-    function flightDetailsCheck(
+    function flightInsuranceDetailsCheck(
         bytes memory _policyKey,
         bytes memory _passengerKey
     ) public view returns(string memory,uint256,uint256){
         FlightInsurance memory fin = insurances[_policyKey][msg.sender];
         // require(insurances[_policyKey][msg.sender].isExist == true, "ERROR:FDI-FDC-01:");
-        require(insurances[_policyKey][passenger[_passengerKey].passengerAddress].flightStatus == FlightStatus(0), "ERROR:FDI-FDC-01:FLIGHT_UNSCHEDULED");
+        require(insurances[_policyKey][passenger[_passengerKey].passengerAddress].flightStatus == FlightStatus(1), "ERROR:FDI-FDC-01:FLIGHT_UNSCHEDULED");
         return (fin.flightIcao,fin.departureOn,fin.arrivalOn);   
     }
 
